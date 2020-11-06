2724093b6294c3d07fd3d0195843fc903be0efe0
luccasbarros <ldbcandido@gmail.com>
2020-11-06 15:57:54 -0300

    creates ok

--- src/endpoint/createStudent.ts
+++ src/endpoint/createStudent.ts
@@ -1,21 +1,24 @@
 import {Request, Response} from "express"
 import { createNewStudent } from "../data/createStudent";
+import { insertStuMission } from "../data/insertStuMission";
 
 
 export const createStudent = async (req: Request, res: Response): Promise<void> => {
+   
    try {
 
       const {id, name, email, hobbies, birthdate, mission_id} = req.body;
 
-     await createNewStudent(id, name, email, hobbies, birthdate, mission_id)
-
-
       if (!id || !name || !email || !hobbies || !birthdate || !mission_id) {
          res.statusCode = 409
-         throw new Error("Missing arguments!")
+         throw new Error("Parâmetros inválidos.")
       }
 
-      res.status(201).send("New Student created!");
+      await createNewStudent(id, name, email, hobbies, birthdate, mission_id)
+      await insertStuMission(id, mission_id)
+
+      res.status(201).send("Estudante criado com sucesso!");
+
    } catch (err) {
     res.status(400).send({
         message: err.message || err.sqlMessage
